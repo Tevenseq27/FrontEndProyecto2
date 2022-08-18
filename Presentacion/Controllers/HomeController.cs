5a40c49df436d87f2b7f2da0c5653eba3d96b840
TEVENSEQ27 <stevensequeira27@gmail.com>
2022-08-17 22:33:18 -0600

    Consulta Rutas

--- Presentacion/Controllers/HomeController.cs
+++ Presentacion/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Presentacion.Models;
-using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using RegistroLogin.Filters;
 using RegistroLogin.Models;
